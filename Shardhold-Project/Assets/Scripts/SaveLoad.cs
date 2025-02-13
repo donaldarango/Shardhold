6c7e26dea8fc731f697e6b3e78cc719c8d4ecbbc
cbakshi314 <100709261+cbakshi314@users.noreply.github.com>
2025-02-13 13:06:39 -0500

    Implemented JSON save files.
    Implemented JSON save files, refactored some areas to improve organization, and implemented reading/writing of MapGenerator's basic map information, namely the number of directions and the number of distances. Does not save terrains, TileActors, base HP/weapon status, or other effects as these do not yet have a way to be accessed that I can tell.

--- Shardhold-Project/Assets/Scripts/SaveLoad.cs
+++ Shardhold-Project/Assets/Scripts/SaveLoad.cs
@@ -5,6 +5,8 @@ using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using static UnityEngine.JsonUtility;
+using Unity.VisualScripting;
+using System.Data;
 
 public class SaveLoad : MonoBehaviour
 {
@@ -15,14 +17,15 @@ public class SaveLoad : MonoBehaviour
     #endregion
 
     public static SaveLoad saveLoad;
+    public MapGenerator mapGenerator;
 
     public enum SaveType{
         chooseDefault,
-        binary
-        //TODO: json
+        binary,
+        json
     }
 
-    public SaveType defaultSaveType = SaveType.binary;
+    public SaveType defaultSaveType = SaveType.json;
     public string fileToUse = "";   //default save file
     public string saveFolder;  //where save files go
 
@@ -53,6 +56,14 @@ public class SaveLoad : MonoBehaviour
         if(saveLoad == null)
         {
             saveLoad = this;
+        }
+    }
+
+    public void RanUnimplementedCode(string descriptor = "<no description>")
+    {
+        if (debugging)
+        {
+            Debug.Log("WARNING: A section of code has just been executed that was not completely impelement: " + descriptor + "\nIf this section was completed, please locate the section and remove the RanUnimplementedCode() function call.");
         }
     }
 
@@ -95,13 +106,65 @@ public class SaveLoad : MonoBehaviour
         if(debugging){
             Debug.Log("Saving game to specified file \"" + fileToUse + "\"");
         }
-        if(saveType == SaveType.chooseDefault){
+
+
+        GameStateData data = new GameStateData();
+        #region Copy game state data to GameStateData
+
+        RanUnimplementedCode("Terrains, base effects and map effects, and TileActors are not yet saveable (or loadable).");
+
+        #region Map Info
+
+        data.sectorCount = getSectionCount();
+        data.maxVisibleRange = getCircleCount() - 1;
+        data.maxActualRange = getCircleCount();
+        data.curTurn = getCurTurn();
+
+        //TODO iterate over the map to gather terrain data
+        for (int i = 0; i < data.maxActualRange; i++)
+        {
+            for (int j = 0; j < data.sectorCount; j++)
+            {
+                //TODO
+            }
+        }
+
+        #endregion
+
+        #region Base
+        data.baseHP = getBaseHP();
+        data.baseWeapon = getBaseWeapon();
+        //include active base defenses/effects here; TODO
+        #endregion
+
+        #region TileActors
+
+        //TODO iterate over all TileActors, adding all the data for each TileActor before moving on to the next TileActor
+        List<TileActor.ObjType> tileActorTypes = new List<TileActor.ObjType>();
+        List<int> ta_spawnTurn = new List<int>();       //the turn on which this TileActor did/will spawn; mainly important for enemies which have not yet spawned
+        List<Vector2Int> ta_pos = new List<Vector2Int>();     //position of tileActors stored as (direction, range)
+        List<int> ta_faction = new List<int>();         //0 for defender, 1 for attacker
+        List<int> ta_maxHealth = new List<int>();       //the max possible health for this TileActor; generally the health that the TileActor spawns with
+        List<int> ta_curHealth = new List<int>();       //the current health of the TileActor; generally maxHealth - damageTaken
+        List<int> ta_damage = new List<int>();          //the standard attack damage of this TileActor
+
+        //special abilities: TODO
+        #endregion
+
+        data.initialized = true;
+
+        #endregion
+
+
+        if (saveType == SaveType.chooseDefault){
             saveType = defaultSaveType;
         }
         switch(saveType){
             case SaveType.binary:
-                return BinarySave(filename);
-                //break;
+                return BinarySave(filename, data);
+            //break;
+            case SaveType.json:
+                return JsonSave(filename, data);
             default:
                 if(debugging){
                     Debug.LogError("Unidentified save type!");
@@ -110,63 +173,49 @@ public class SaveLoad : MonoBehaviour
         }
     }
 
-    //TODO replace with json save system
-    private bool BinarySave(string filename){
+    //this has been replaced with json file save system
+    private bool BinarySave(string filename, GameStateData data){
         try{
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Create(saveFolder + "/" + filename);
 
-            GameStateData data = new GameStateData();
-
-            #region Copy game state data to GameStateData
-            
-            #region Map Info
-
-            data.sectorCount = 4;
-            data.maxVisibleRange = 5;
-            data.maxActualRange = 6;
-            data.curTurn = -1;  //TODO
-
-            //TODO iterate over the map to gather terrain data
-            for(int i = 0; i < data.maxActualRange; i++){
-                for(int j = 0; j < data.sectorCount; j++){
-                    //TODO
-                }
-            }
-
-            #endregion
-
-            #region Base
-            data.baseHP = -1;
-            data.baseWeapon = -1;
-            //include active base defenses/effects here; TODO
-            #endregion
-
-            #region TileActors
-
-            //TODO iterate over all TileActors, adding all the data for each TileActor before moving on to the next TileActor
-            List<TileActor.ObjType> tileActorTypes = new List<TileActor.ObjType>();
-            List<int> ta_spawnTurn = new List<int>();       //the turn on which this TileActor did/will spawn; mainly important for enemies which have not yet spawned
-            List<Vector2Int> ta_pos = new List<Vector2Int>();     //position of tileActors stored as (direction, range)
-            List<int> ta_faction = new List<int>();         //0 for defender, 1 for attacker
-            List<int> ta_maxHealth = new List<int>();       //the max possible health for this TileActor; generally the health that the TileActor spawns with
-            List<int> ta_curHealth = new List<int>();       //the current health of the TileActor; generally maxHealth - damageTaken
-            List<int> ta_damage = new List<int>();          //the standard attack damage of this TileActor
-
-            //special abilities: TODO
-            #endregion
-            #endregion
-
-
             bf.Serialize(file, data);
             file.Close();
+
             if(debugging){
                 Debug.Log("Current game has been saved to \"" + filename + "\" in folder \"" + saveFolder);
             }
             return true;
         }catch(Exception e){
             if(debugging){
-                Debug.LogError("ERROR WHEN SAVING: " + e.Message);
+                Debug.LogError("ERROR WHEN SAVING BINARY: " + e.Message);
+            }
+            return false;
+        }
+    }
+
+    private bool JsonSave(string filename, GameStateData data)
+    {
+        try
+        {
+            //convert game data into a json
+            string jsonData = ToJson(data, true);
+
+            //save the json string into a file
+            WriteFile(filename, jsonData);
+
+            //success, presumably
+            if (debugging)
+            {
+                Debug.Log("Current game has been saved to \"" + filename + "\" in folder \"" + saveFolder);
+            }
+            return true;
+        }
+        catch (Exception e)
+        {
+            if (debugging)
+            {
+                Debug.LogError("ERROR WHEN SAVING JSON: " + e.Message);
             }
             return false;
         }
@@ -176,40 +225,47 @@ public class SaveLoad : MonoBehaviour
         if(debugging){
             Debug.Log("Loading game from specified file \"" + fileToUse + "\"");
         }
+
+        GameStateData data = new GameStateData();
+        bool noProblemLoading = true;   //set to false if there are any issues with the loading process
+
         if(saveType == SaveType.chooseDefault){
             saveType = defaultSaveType;
         }
         switch(saveType){
             case SaveType.binary:
-                return BinaryLoad(filename);
-                //break;
+                noProblemLoading = BinaryLoad(filename, out data);
+                break;
+            case SaveType.json:
+                noProblemLoading = JsonLoad(filename, out data);
+                break;
             default:
                 if(debugging){
                     Debug.LogError("Unidentified save type!");
                 }
-                return false;
+                noProblemLoading = false;
+                break;
         }
-    }
 
-    //TODO replace with json save system here as well
-    private bool BinaryLoad(string filename){
-        //remove all current stuff from the board in preparation for the new stuff
-        Unload();
-        if(File.Exists(saveFolder + "/" + filename)){
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(saveFolder + "/" + filename, FileMode.Open);
-            GameStateData data = (GameStateData)bf.Deserialize(file);
-            file.Close();
-
+        noProblemLoading = noProblemLoading && data != null;
+        if (noProblemLoading)
+        {
             #region turn the GameStateData object into the actual state of the game
 
-            #region Overall Game Info
+            #region Overall Game/Map Info
 
-            //TODO map stuff other than terrain, see BinarySave
+            RanUnimplementedCode("Terrains, base effects and map effects, and TileActors are not yet loadable (or saveable).");
+
+            //general map stuff
+            setSectionCount(data.sectorCount);
+            setCircleCount(data.maxActualRange);
+            setCurTurn(data.curTurn);
 
             //TODO iterate over the map to gather terrain data
-            for(int i = 0; i < data.maxActualRange; i++){
-                for(int j = 0; j < data.sectorCount; j++){
+            for (int i = 0; i < data.maxActualRange; i++)
+            {
+                for (int j = 0; j < data.sectorCount; j++)
+                {
                     //TODO
                 }
             }
@@ -217,8 +273,9 @@ public class SaveLoad : MonoBehaviour
             #endregion
 
             #region Base
-            //TODO
-            //include active base defenses/effects here; TODO
+            setBaseHP(data.baseHP);
+            setBaseWeapon(data.baseWeapon);
+            //TODO: include active base defenses/effects here
             #endregion
 
             #region TileActors
@@ -229,20 +286,63 @@ public class SaveLoad : MonoBehaviour
             #endregion
 
             #endregion
+        }
+        return noProblemLoading;
+
+    }
+
+    //this has also been replaced with json file save system, like the save system
+    private bool BinaryLoad(string filename, out GameStateData data){
+        //remove all current stuff from the board in preparation for the new stuff
+        Unload();
+        if(File.Exists(saveFolder + "/" + filename)){
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(saveFolder + "/" + filename, FileMode.Open);
+            data = (GameStateData)bf.Deserialize(file);
+            file.Close();
+
 
             if(debugging){
-                Debug.Log("Successfully loaded file.");
+                Debug.Log("Current game has been loaded from \"" + filename + "\" in folder \"" + saveFolder);
             }
             return true;
         }else{
             if(debugging){
                 Debug.LogError("Error with file; failed to load.");
             }
+            data = null;
             return false;
         }
     }
 
-	
+	private bool JsonLoad(string filename, out GameStateData data)
+    {
+        try
+        {
+            //read the file into a string
+            string jsonData = ReadFile(filename);
+
+            //convert json into GameStateData object
+            data = FromJson<GameStateData>(jsonData);
+
+            //presumably the operation was successful
+            if (debugging)
+            {
+                Debug.Log("Current game has been loaded from \"" + filename + "\" in folder \"" + saveFolder);
+            }
+            return true;
+        }
+        catch (Exception e)
+        {
+            if (debugging)
+            {
+                Debug.LogError("ERROR WHEN LOADING JSON: " + e.Message);
+            }
+            data = null;
+            return false;
+        }
+    }
+
 	private void Unload()
 	{
 
@@ -255,6 +355,124 @@ public class SaveLoad : MonoBehaviour
 	{
 		//same thing as Load(), but doesn't unload previous things
 	}*/
+
+    #region Getting and Setting of Variables when Saving and Loading
+
+    //get the number of directions on the map
+    private int getSectionCount()
+    {
+        if (mapGenerator == null)
+        {
+            if (debugging)
+            {
+                Debug.LogError("No MapGenerator assigned to SaveLoad.");
+            }
+            return -1;
+        }
+        else
+        {
+            return mapGenerator.sectionCount;
+        }
+    }
+
+    //set the number of directions on the map
+    private void setSectionCount(int amt)
+    {
+        if (mapGenerator == null)
+        {
+            if (debugging) {
+                Debug.LogError("No MapGenerator assigned to SaveLoad.");
+            }
+        }
+        else
+        {
+            mapGenerator.sectionCount = amt;
+        }
+    }
+
+    //get the number of rings around the base; does not include the base but does include invisible spawning rings (currently assumes 1 spawning ring, see load and save functions)
+    private int getCircleCount()
+    {
+        if (mapGenerator == null)
+        {
+            if (debugging)
+            {
+                Debug.LogError("No MapGenerator assigned to SaveLoad.");
+            }
+            return -1;
+        }
+        else
+        {
+            return mapGenerator.circleCount;
+        }
+    }
+
+    //set the number of rings around the base
+    private void setCircleCount(int amt)
+    {
+        if (mapGenerator == null)
+        {
+            if (debugging)
+            {
+                Debug.LogError("No MapGenerator assigned to SaveLoad.");
+            }
+        }
+        else
+        {
+            mapGenerator.sectionCount = amt;
+        }
+    }
+
+    //get the current turn counter
+    private int getCurTurn()
+    {
+        //TODO
+        RanUnimplementedCode("getCurTurn()");
+        return -1;
+    }
+
+    //set the turn counter
+    private void setCurTurn(int curTurn)
+    {
+        //TODO
+        RanUnimplementedCode("setCurTurn");
+    }
+
+    //get the health of the base
+    private int getBaseHP()
+    {
+        //TODO
+        RanUnimplementedCode("getBaseHP()");
+        return -1;
+    }
+
+    //set the base HP
+    private void setBaseHP(int hp)
+    {
+        //TODO
+        RanUnimplementedCode("setBaseHP()");
+    }
+
+    //get the current damage value of the base weapon
+    private int getBaseWeapon()
+    {
+        //TODO
+        RanUnimplementedCode("getBaseWeapon()");
+        return -1;
+    }
+
+    //set the current base weapon damage
+    private void setBaseWeapon(int amt)
+    {
+        //TODO
+        RanUnimplementedCode("setBaseWeapon()");
+    }
+
+
+    #endregion
+
+
+
     #endregion
 
     #region Player Stats Save/Load
@@ -296,6 +514,8 @@ public class SaveLoad : MonoBehaviour
 class GameStateData
 {
 #pragma warning disable 0649
+
+    public bool initialized = false;    //set to true to indicate that all of the following variables were given the correct information that is wanted for saving/loading
 
     //we will assume that the game is only ever saved on the player's turn
     #region Map Info
