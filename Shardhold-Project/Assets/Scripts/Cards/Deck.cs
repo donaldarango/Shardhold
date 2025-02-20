69e2563f9e7b45406c376f70779fa0cf85715721
cbakshi314 <100709261+cbakshi314@users.noreply.github.com>
2025-02-20 09:45:20 -0500

    More work done on Deck.cs

--- Shardhold-Project/Assets/Scripts/Cards/Deck.cs
+++ Shardhold-Project/Assets/Scripts/Cards/Deck.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using JetBrains.Annotations;
 using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-
+    //this will be used for converting int into a Card
+    List<Card> cardLookup = new List<Card>();
 
     //stores what has been unlocked for the player; is not the actual deck necessarily
     //each card is an index value; the value at that index is the number of copies of that card unlocked
@@ -25,12 +27,18 @@ public class Deck : MonoBehaviour
     //uses same rules as drawPile
     List<int> hand = new List<int>();
 
-
+    /// <summary>
+    /// returns the number of cards in the hand
+    /// </summary>
+    /// <returns></returns>
     public int CountCardsInHand()
     {
         return hand.Count;
     }
 
+    /// <summary>
+    /// To be used at the beginning of the player's turn to fill their hand
+    /// </summary>
     public void DrawCardsUntilFull()
     {
         int safety = 200;
@@ -41,6 +49,9 @@ public class Deck : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// move a card from the draw pile to the hand
+    /// </summary>
     public void DrawCard()
     {
         //choose a card from the draw pile
@@ -51,9 +62,17 @@ public class Deck : MonoBehaviour
 
         //remove from draw pile
         drawPile.RemoveAt(choice);
+
+        if(drawPile.Count <= 0)
+        {
+            SwapDrawAndDiscard();
+        }
     }
 
-    //remove a card from the hand based on its position in the hand list and send it to the discard pile
+    /// <summary>
+    /// remove a card from the hand based on its position in the hand list and send it to the discard pile
+    /// </summary>
+    /// <param name="handPosition">Which card to discard; refers to position in hand, *NOT* the card as an int representation</param>
     public void DiscardCard(int handPosition)
     {
         //send to discard pile
@@ -62,4 +81,58 @@ public class Deck : MonoBehaviour
         //remove from hand
         hand.RemoveAt(handPosition);
     }
+
+    /// <summary>
+    /// use this when draw pile is empty to send the discard pile to the draw pile (and vice versa)
+    /// </summary>
+    public void SwapDrawAndDiscard()
+    {
+        List<int> temp = drawPile;
+        drawPile = discardPile;
+        discardPile = temp;
+    }
+
+    /// <summary>
+    /// Check if the given draw pile is possible based on what cards are unlocked; should only be used when hand and discard pile are empty
+    /// </summary>
+    /// <returns>true: pile is valid; false: pile is invalid, containing some cards that are not unlocked OR more cards of a kind than are unlcoked of that kind</returns>
+    public bool ValidatePile()
+    {
+        //TODO
+        if (CustomDebug.DeckDebugging())
+        {
+            CustomDebug.RanUnimplementedCode("ValidatePile");
+        }
+        return true;
+    }
+
+    #region Gets, Sets, etc.
+
+    /// <summary>
+    /// Return a list of card objects that are in the player's hand
+    /// </summary>
+    /// <returns></returns>
+    public List<Card> GetCardsInHand()
+    {
+        List <Card> cards = new List<Card>();
+        for (int i = 0; i < CountCardsInHand(); i++)
+        {
+            cards.Add(cardLookup[i]);
+        }
+        return cards;
+    }
+
+    /// <summary>
+    /// get the Card that is at a certain position in the hand
+    /// </summary>
+    /// <param name="handPosition">The position in the hand to investigate</param>
+    /// <returns></returns>
+    public Card GetCardInHandPos(int handPosition)
+    {
+        int cardInt = hand[handPosition];   //get the int representation of the card at the given position in the hand
+        Card card = cardLookup[cardInt];    //turn the int representation into an actual Card object
+        return card;
+    }
+
+    #endregion
 }
