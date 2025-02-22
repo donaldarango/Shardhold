b06fc8b77f06a2113d530e24b77f5f096e4490f2
Sonny-Cao <sonnycao@ufl.edu>
2025-02-22 03:33:13 -0500

    Card prefab and timer
    Card Prefab and layout done, timer UI scrollbar with time implemented, need to add sprites/refine timer to make it look better

--- Shardhold-Project/Assets/Scripts/UI/TurnTimer.cs
+++ Shardhold-Project/Assets/Scripts/UI/TurnTimer.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections.Specialized;
+using System.Threading;
+using NUnit.Framework.Internal.Commands;
 public class TurnTimer : MonoBehaviour
 {
     [SerializeField]
     public static float time = 10.0f;
+    public float resetTime; 
+    public Gradient gradient;
+    public Image RightBar;
+    public Image LeftBar;
+
     public TMP_Text timeText;
+    public Slider sliderRight;
+    public Slider sliderLeft;
     public bool playerTurn = false;
+    //Consider an audio clip that plays in the last few seconds like a league of legends champ select countdown
+    //Consider a lock in/turn switch sound when time hits 0
 
     void Start()
     {
@@ -20,22 +31,31 @@ public class TurnTimer : MonoBehaviour
             string timerInt = time.ToString("0");
             timeText.text = timerInt;
             time -= Time.deltaTime;
+            sliderRight.value -= Time.deltaTime;
+            sliderLeft.value -= Time.deltaTime;
+            RightBar.color = gradient.Evaluate(time);
+            LeftBar.color = gradient.Evaluate(time);
+
         }
-        if (time <= 0) {
+        if (time <= 0 && playerTurn == true) {
             EndPlayerTurn();
         }
     }
 
     public void EndPlayerTurn()
     {
-        Debug.Log("Player turn started");
+        Debug.Log("Player turn ended");
         playerTurn = false;
-        timeText.text = " ";
+        timeText.text = "";
     }
     public void StartPlayerTurn()
     {
         Debug.Log("Player turn started");
-        time = 10.0f;
+        time = resetTime;
+        sliderLeft.maxValue = resetTime;
+        sliderLeft.maxValue = resetTime;
+        sliderLeft.value = resetTime;
+        sliderRight.value = resetTime;
         playerTurn = true;
     }
 }
